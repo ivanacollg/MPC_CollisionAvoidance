5f8fdbd0a1d199915e1a57723eb904ecb979d118
IvanaColl <ivanacollado@gmail.com>
2021-03-25 07:44:02 -0600

    guidance 3 with 1.5 current

--- catkin_ws/src/nmpc_ca/scripts/usv_guidance3/acados_settings.py
+++ catkin_ws/src/nmpc_ca/scripts/usv_guidance3/acados_settings.py
@@ -72,12 +72,12 @@ def acados_settings(Tf, N):
     #nsh = 2
 
     # set cost
-    Q = np.diag([0, 0, 0, 0.05, 0.05, 0, 0, 0.0, 0.1, 0, 0.1])
+    Q = np.diag([0, 0, 0, 0.05, 0.05, 0, 0, 0.0, 0.07, 0, 0.1])
     
     R = np.eye(nu)
     R[0, 0] = 0.03
 
-    Qe = np.diag([0, 0, 0, 0.1, 0.1, 0, 0, 0.0, 0.5, 0, 0.2])
+    Qe = np.diag([0, 0, 0, 0.1, 0.1, 0, 0, 0.0, 0.2, 0, 0.2])
 
 
     ocp.cost.cost_type = "LINEAR_LS"
